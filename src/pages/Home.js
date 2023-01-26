5aae15a8eaf48b86458495dc4499204633d20943
Ryanito <ryan.cornelio@gmail.com>
2023-01-26 22:59:58 +0100

    Good

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,17 @@ import "./Home.css"
 
 const Home = () => {
   return (
+    <>
     <div className='hero-section'>
-    Home Page
+    <h1 className='headline'>Born 1991 in Gothenburg, Sweden.</h1>
+    <h1 className='headline1'>Born 1991 in Gothenburg, Sweden.</h1>
+    <h1 className='headline2'>Born 1991 in Gothenburg, Sweden.</h1>
+    <h1 className='headline3'>Born 1991 in Gothenburg, Sweden.</h1>
     </div>
+    <p className='textbox'>
+      Hej!
+    </p>
+    </>
   )
 }
 
