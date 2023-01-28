8108b7413ff1e3ae8ceeae04014a27144fbefb9e
Ryanito <ryan.cornelio@gmail.com>
2023-01-28 17:07:52 +0100

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppBar, Collapse, IconButton, Toolbar } from '@mui/material';
 import SortIcon from '@mui/icons-material/Sort';
 import './Header.css';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const Header = () => {
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  }, [])
   return (
-    <div className="App">
-      <AppBar style={{ background: 'transparent', boxShadow: 'none', fontFamily: 'nunito'}}>
+    <div className="root">
+      <AppBar style={{ background: 'transparent', boxShadow: 'none', fontFamily: 'nunito' }}>
         <Toolbar className='appbarWrapper'>
-        <h1 className="appbarTitle">My<span className="colorText">Island.</span></h1>
-        <IconButton>
-            <SortIcon className='icon'/>
-        </IconButton>
+          <h1 className="appbarTitle">Ryan<span className="colorText">Cornelio</span></h1>
+          <IconButton>
+            <SortIcon className='icon' />
+          </IconButton>
         </Toolbar>
       </AppBar>
 
-      <div>
-        <h1>Welcome To <br/> My <span className="colorText">Island</span></h1>
-      </div>
-
+      <Collapse in={checked}
+        {...(checked ? { timeout: 1000 } : {})}
+        collapsedHeight={50}
+      >
+        <div className="container">
+          <h1 className="title">Welcome To <br /> My <span className="colorText">Page</span>
+        </h1>
+          <IconButton>
+            <KeyboardArrowDownIcon style={{ fontSize: '3em'}} className="goDown"/>
+          </IconButton>
+        </div>
+      </Collapse>
     </div>
   )
 }
